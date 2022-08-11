9f5f9a935ac0ef7b6d30b31b08353eda1b3be1ff
Prathamesh <110330280+Prathamesh0401@users.noreply.github.com>
2022-08-11 14:43:18 +0530

    Add files via upload

---
+++ My Second Program.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+import pandas as pd
+
+
+# In[7]:
+
+
+pd.read_excel(r'C:\Users\Prathmesh Lanjekar\Desktop\Data 1.xlsx')
+
+
+# In[8]:
+
+
+import os
+
+
+# In[9]:
+
+
+os.getcwd()
+
+
+# In[10]:
+
+
+import pandas as pd
+
+
+# In[13]:
+
+
+pd.read_csv('August 6.txt',sep='t')
+
+
+# In[ ]:
+
+
+
+
